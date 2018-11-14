b1097c47557a91c09ee2d2c2e1a16bd53808c174
plusect <plusect@outlook.com>
2018-11-14 23:45:44 +0800

    插入排序

--- src/cn/plusect/Sort.java
+++ src/cn/plusect/Sort.java
@@ -12,21 +12,22 @@ public class Sort {
 
     /**
      * 冒泡排序
+     *
      * @param a
      * @return
      */
-    private static int[] bubbleSort(int[] a){
+    private static int[] bubbleSort(int[] a) {
         int length = a.length;
-        if (length <=1){
+        if (length <= 1) {
             return a;
         }
         for (int i = 0; i < length; i++) {
             //提前退出冒泡循环的标志位
             boolean flag = false;
-            for (int j = 0; j < length - i - 1; j++){
-                if (a[j]>a[j+1]){
-                    int tmp = a[j+1];
-                    a[j+1] = a[j];
+            for (int j = 0; j < length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j + 1];
+                    a[j + 1] = a[j];
                     a[j] = tmp;
                     flag = true; //表示数据交换
                 }
@@ -36,8 +37,21 @@ public class Sort {
         return a;
     }
 
-    private static int[] insertionSort(int[] a){
-
+    private static int[] insertionSort(int[] a) {
+        int length = a.length;
+        if (length <= 1) return a;
+        for (int i = 0; i < length; i++) {
+            int value = a[i];
+            int j = i - 1;
+            for (; j >= 0; --j) {
+                if (a[j] > value) {
+                    a[j + 1] = a[j];
+                } else {
+                    break;
+                }
+            }
+            a[j + 1] = value;
+        }
         return a;
     }
 }
