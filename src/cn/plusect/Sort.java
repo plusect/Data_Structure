ea3ba04b218b1afcfa37d2e644064c275217b90e
LAsuka <plusect@outlook.com>
2018-11-21 16:41:46 +0800

    桶排序改为特殊的一类：计数排序

--- src/cn/plusect/Sort.java
+++ src/cn/plusect/Sort.java
@@ -13,16 +13,17 @@ public class Sort {
         System.out.println("Selection Sort: " + Arrays.toString(selectionSort(new int[]{5, 4, 2, 7, 1})));
         System.out.println("Merge Sort: " + Arrays.toString(mergeSort(new int[]{5, 4, 2, 7, 1})));
         System.out.println("Quick Sort: " + Arrays.toString(quickSort(new int[]{5, 4, 2, 7, 1})));
-        System.out.println("Bucket Sort: " + Arrays.toString(bucketSort(new int[]{5, 4, 2, 7, 1})));
+        System.out.println("Counting Sort: " + Arrays.toString(countingSort(new int[]{5, 4, 2, 7, 1})));
     }
 
     /**
-     * 桶排序：
+     * 计数排序：
      * @param a
      * @return
      */
-    private static int[] bucketSort(int[] a) {
-        return new int[0];
+    private static int[] countingSort(int[] a) {
+
+        return a;
     }
 
     /**
