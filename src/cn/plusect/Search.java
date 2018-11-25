fb899464a8812c442a963db2640d14e10f9fce06
plusect <plusect@outlook.com>
2018-11-25 23:28:19 +0800

    添加first index for value的二分查找

--- src/cn/plusect/Search.java
+++ src/cn/plusect/Search.java
@@ -7,10 +7,38 @@ import java.util.Arrays;
  */
 public class Search {
     public static void main(String[] args) {
-        System.out.println("Simple Binary Search without recursion to get index for value :2 from " + Arrays.toString(new int[]{5, 4, 2, 7, 1})
-                + ",the result:" + simpleBinarySearchByWithoutRecursion(new int[]{5, 4, 2, 7, 1}, 2));
-        System.out.println("Simple Binary Search with recursion to get index for value:2 from " + Arrays.toString(new int[]{5, 4, 2, 7, 1})
-                + ",the result:" + simpleBinarySearchByWithRecursion(new int[]{5, 4, 2, 7, 1}, 2));
+        System.out.println("Simple Binary Search without recursion to get index for value :2 from " + Arrays.toString(new int[]{1, 2, 3, 4, 5})
+                + ",the result:" + simpleBinarySearchByWithoutRecursion(new int[]{1, 2, 3, 4, 5}, 2));
+        System.out.println("Simple Binary Search with recursion to get index for value:2 from " + Arrays.toString(new int[]{1, 2, 3, 4, 5})
+                + ",the result:" + simpleBinarySearchByWithRecursion(new int[]{1, 2, 3, 4, 5}, 2));
+        System.out.println("Binary Search to get first index for value:2 from " + Arrays.toString(new int[]{1, 2, 3, 4, 5})
+                + ",the result:" + binarySearchForFirstIndex(new int[]{1, 2, 2, 3, 4, 5}, 2));
+    }
+
+    /**
+     * 使用二分法查找first index for value
+     * @param a
+     * @param value
+     * @return
+     */
+    private static int binarySearchForFirstIndex(int[] a, int value) {
+        int length = a.length;
+        int low = 0;
+        int high = length - 1;
+        while (low <= high){
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] == value){
+                if (a[mid -1] < value)
+                    return mid;
+                else
+                    return -1;
+            }else if (a[mid] > value){
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 
     /**
