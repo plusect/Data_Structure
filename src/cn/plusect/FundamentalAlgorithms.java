5b42c7c48e26d1f01494d1f748bb9a8d8f887174
LAsuka <plusect@outlook.com>
2019-03-25 17:11:24 +0800

    0-1 knapsack to Dynamic Programming init

--- src/cn/plusect/FundamentalAlgorithms.java
+++ src/cn/plusect/FundamentalAlgorithms.java
@@ -5,5 +5,14 @@ package cn.plusect;
  * @author lianzb
  */
 public class FundamentalAlgorithms {
-
+    /**
+     * 动态规划，背包0-1问题
+     * @param weight 物品重量
+     * @param n 物品个数
+     * @param w 背包可承载重量
+     * @return 最大重量
+     */
+    public int knapsack(int[] weight, int n, int w) {
+        return -1;
+    }
 }
